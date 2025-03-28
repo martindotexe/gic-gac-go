49d010ae3ac5a49505a67f93aec6a1feb5fb2198
martindotexe <martin@solhaugfoto.no>
2025-03-28 21:31:04 +0100

    Fixed bug in Player function and added playable game to main

--- game/game.go
+++ game/game.go
@@ -25,9 +25,56 @@ func NewGame() *Game {
 	return new(Game)
 }
 
+func allEqual[T comparable](items ...T) bool {
+	for i := 1; i < len(items); i++ {
+		if items[i] != items[0] {
+			return false
+		}
+	}
+	return true
+}
+
+func (g *Game) Winner() Player {
+	// Check rows
+	for i := range 3 {
+		x, _ := g.Get(0, i)
+		y, _ := g.Get(1, i)
+		z, _ := g.Get(2, i)
+		if allEqual(x, y, z) && x != Empty {
+			return x
+		}
+	}
+
+	// Check columns
+	for i := range 3 {
+		x, _ := g.Get(i, 0)
+		y, _ := g.Get(i, 1)
+		z, _ := g.Get(i, 2)
+		if allEqual(x, y, z) && x != Empty {
+			return x
+		}
+	}
+
+	// Check diagonals
+	x, _ := g.Get(0, 0)
+	y, _ := g.Get(1, 1)
+	z, _ := g.Get(2, 2)
+	if allEqual(x, y, z) && x != Empty {
+		return x
+	}
+	x, _ = g.Get(0, 2)
+	y, _ = g.Get(1, 1)
+	z, _ = g.Get(2, 0)
+	if allEqual(x, y, z) && x != Empty {
+		return x
+	}
+
+	return Empty
+}
+
 func (g *Game) Player() Player {
 	// Reads the 19th bit of the game state to determine the player
-	return Player((*g >> 18) & 1)
+	return Player((*g>>18)&1) + 1
 }
 
 func (g *Game) TogglePlayer() {
--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
 	"martindotexe/tic-tac-toe/game"
 )
 
 func main() {
 	g := game.NewGame()
 
-	err := g.Set(1, 1, game.Player1)
-
-	if err != nil {
-		slog.Error(err.Error())
+	for g.Winner() == game.Empty {
+		fmt.Println(g)
+		fmt.Println("Enter your move:")
+		var x, y int
+		_, err := fmt.Scan(&x, &y)
+		if err != nil {
+			panic(err)
+		}
+		err = g.Set(x, y, g.Player())
+		if err != nil {
+			panic(err)
+		}
+		g.TogglePlayer()
 	}
-
-	fmt.Println(g)
+	fmt.Println(g.Winner(), "wins!")
 }
